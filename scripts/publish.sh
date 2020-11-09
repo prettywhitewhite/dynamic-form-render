73d2980cd15d24ade0f2c05914dc178ac1f76e2e
chunmei6 <chunmei6@staff.weibo.com>
2020-11-09 18:54:26 +0800

    chore(release) version: 1.0.7 commit:

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -14,7 +14,7 @@ cd docs/.vuepress/dist
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'deploy-docs'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
